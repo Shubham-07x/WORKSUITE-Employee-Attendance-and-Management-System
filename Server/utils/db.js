d7af36226a92693c19ad7ac24f3d8ac65014c00a
Shubham Sahu <sahubanti744@gmail.com>
2024-10-07 09:52:31 +0530

    Update db.js

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -1,12 +1,12 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
 
-dotenv.config(); // Load environment variables from .env file
+dotenv.config(); 
 
 const db = new pg.Client({
   connectionString: process.env.POSTGRES_URL,
   ssl: {
-    rejectUnauthorized: false, // For self-signed certificates; adjust as necessary
+    rejectUnauthorized: false,
   },
 });
 
